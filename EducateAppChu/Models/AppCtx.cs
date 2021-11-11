01294862ebcc4f974dc4ce1d0a3355caa5c6580c
lizug <lizug@DESKTOP-GCN26MP>
2021-11-11 12:54:53 +0500

    Добавьте файлы проекта.

---
+++ EducateAppChu/Models/AppCtx.cs
@@ -0,0 +1,15 @@
+﻿using EducateAppChu.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducateApp.Models
+{
+    public class AppCtx : IdentityDbContext<User>
+    {
+        public AppCtx(DbContextOptions<AppCtx> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ EducateAppChu/Models/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace EducateAppChu.Models
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "lizuha-lis2-2002@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.mail.ru", 465, true);
+                await client.AuthenticateAsync("lizuha-lis2-2002@mail.ru", "SiMbb0pbXRyK8H6gD53N");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
